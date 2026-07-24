15a4396b7232776662e67ca27b8685fde7742423
Harikrishnan1122001 <hk1396897@gmail.com>
2026-07-24 16:55:51 +0530

    new code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import SPDExports from "./SPDExports.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <SPDExports />
   </React.StrictMode>
 );
